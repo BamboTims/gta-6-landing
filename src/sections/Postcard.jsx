ec09a727269a2ef132ddda9caa6c853f7bc63760
BamboTims <timilehinadebambo@gmail.com>
2025-08-10 14:42:56 +0100

    Add Postcard component to App; integrate new section into layout

---
+++ src/sections/Postcard.jsx
@@ -0,0 +1,53 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef } from "react";
+
+const Postcard = () => {
+  const videoRef = useRef(null);
+
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".post-card",
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+
+    videoRef.current.onloadedemetadata = () => {
+      tl.to(
+        videoRef.current,
+        {
+          currentTime: videoRef.current.duration,
+          duration: 3,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+    };
+  });
+
+  return (
+    <section className="post-card">
+      <div className="animated-gradient-bg" />
+
+      <div className="post-card-wrapper group hover:rotate-1 hover:-[1.02] transition duration-700">
+        <img src={`${import.meta.env.BASE_URL}images/overlay.webp`} />
+        <video
+          ref={videoRef}
+          muted
+          playsInline
+          autoPlay
+          preload="auto"
+          src={`${import.meta.env.BASE_URL}videos/postcard-vd.mp4`}
+        />
+        <button className="group-hover:bg-yellow transition duration-700">
+          Explore Leonida Keys
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Postcard;
