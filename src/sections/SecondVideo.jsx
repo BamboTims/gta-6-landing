45ded7e66df076c7b34288eff98b8c5280a92a5f
BamboTims <timilehinadebambo@gmail.com>
2025-08-10 11:33:12 +0100

    Add GSAP animations to SecondVideo component for scroll-triggered effects

--- src/sections/SecondVideo.jsx
+++ src/sections/SecondVideo.jsx
@@ -1,7 +1,40 @@
 import React, { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const SecondVideo = () => {
   const videoRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.set(".lucia", {
+      marginTop: "-60vh",
+      opacity: 0,
+    });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".lucia",
+        start: "top top",
+        end: "bottom top",
+        scrub: 2,
+        pin: true,
+      },
+    });
+
+    tl.to(".lucia", { opacity: 1, duration: 1, ease: "power1.inOut" });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(
+        videoRef.current,
+        {
+          currentTime: videoRef.current.duration,
+          duration: 3,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+    };
+  });
 
   return (
     <section className="lucia">
