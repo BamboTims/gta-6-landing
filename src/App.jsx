4dfde73a743cdd17a5b2a8ad2c0bebfe60b3b1f8
BamboTims <timilehinadebambo@gmail.com>
2025-08-10 09:49:01 +0100

    Sure! Pl

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/all";
+
+const App = () => {
+  return (
+    <main>
+      <dir>
+        <h1 className="text-3xl text-red-300">Wakanda</h1>
+      </dir>
+    </main>
+  );
+};
+
+export default App;
